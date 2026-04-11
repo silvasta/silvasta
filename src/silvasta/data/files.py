438f7ed9e0dc74600459fe18602d979936d7d489
silvan <silvasta3@gmail.com>
2026-04-11 19:10:36 +0200

    Files

--- src/silvasta/data/files.py
+++ src/silvasta/data/files.py
@@ -1,29 +1,58 @@
 from datetime import datetime
+from pathlib import Path
 
+from loguru import logger
 from pydantic import BaseModel, Field
 
+# LATER:
+# - first_invalid / last valid
+# - updated as list of updates:
+#   - last_updated as property
+#   - to heavy? force len(update_list) ~ 1
+#   - still to much computation for nothing? check!
 
-class Course(BaseModel):
-    """Course with all client information"""
-
-    # NEXT: check with sachmis and shmoodle
-    # REMOVE: place this here when actually used somewhere else
-    name: str  # Clean name without year, e.g. "Game Theory and Control"
-    id: int = 0  # unique? course ID
-    semester: str = ""  # e.g. "HS25"
-    short: str = ""  # e.g. "GTC" for "Game Theory and Control"
-    is_active: bool = True
+# LATER: status: str|Enum|something_else?
+# - check what kind of status always appear over most projects
 
 
 class SstFile(BaseModel):
     """Local file for upload and usage in prompt"""
 
-    name: str  # TODO: define excact meaning
-    # category: str  # WARN: under development, may change soon
-    # topic: str  # WARN: under development, may change soon
-    # TODO: status: str|Enum|something_else?
-    # TODO: local_path: Path  # relative from local filedir
+    local_path: Path  # relative from local filedir
+    keywords: set = Field(default_factory=set)
 
     # Timestamps
-    created_at: datetime = Field(default_factory=datetime.now)
-    # updated_at: datetime = Field(default_factory=datetime.now) #TODO: how to save?
+    first_tracked: datetime = Field(default_factory=datetime.now)
+    last_updated: datetime = Field(default_factory=datetime.now)
+
+    @property
+    def is_temp_file(self) -> bool:
+        # IDEA: check if maybe needed
+        # - for temporary instance without local_path?
+        return self.local_path == Path(".")
+
+    @property
+    def name(self) -> str:
+        return self.local_path.name
+
+    @property
+    def stem(self) -> str:
+        return self.local_path.stem
+
+    def touch(self) -> datetime:
+        self.last_updated: datetime = datetime.now()
+        return self.last_updated
+
+    # TODO: get file by keyword
+
+    def confirm_local_status(self, local_dir: Path) -> bool:
+        file_path: Path = local_dir / self.local_path
+        if not file_path.is_file():
+            if not file_path.exists():
+                msg: str = f"Nothing found at: {file_path=}"
+            else:
+                msg: str = f"No file found but exists: {file_path=}"
+            logger.warning(msg)
+            return False
+        # LATER: check for changes, hash, etc
+        return True
