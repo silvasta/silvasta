a83be21340d8ca154762ea0e82e4e15b1ea084ff
silvan <silvasta3@gmail.com>
2026-07-25 06:04:15 +0200

    Tui selector

--- src/sstcore/tui/__init__.py
+++ src/sstcore/tui/__init__.py
@@ -1,6 +1,8 @@
 __all__: list[str] = [
     "ListSelectorApp",
     "TreeSelectorApp",
+    "selector",
 ]
+from . import selector
 from .list_selector import ListSelectorApp
 from .tree_selector import TreeSelectorApp
--- src/sstcore/tui/selector.py
+++ src/sstcore/tui/selector.py
@@ -5,3 +5,38 @@ TASK: Collect TuiSelector applications with reusable Pattern
   just throw in any iterable, select, process output, handover
   - check collections from sachmis, flux, robol or others
 """
+
+from loguru import logger
+
+from ..exceptions import TuiSelectorError
+from .list_selector import ListSelectorApp
+
+
+# MOVE: sstcore
+def multi_linear[T](items: dict[T, str] | list[T] | set[T]) -> list[T]:
+    """Select from linear Container and get multiple Elements back"""
+
+    tui = ListSelectorApp(items=items, multi_select=True)
+
+    if selected := tui.run():
+        logger.success(f"Selected {len(selected)} elements")
+        return selected
+
+    raise TuiSelectorError
+
+
+# MOVE: sstcore
+def single_linear[T](items: dict[T, str] | list[T] | set[T]) -> T:
+    """Select from linear Container and get 1 Element back"""
+
+    tui = ListSelectorApp(items=items, multi_select=False)
+
+    if selected := tui.run():
+        item: T = selected[0]
+        logger.success(f"Selected: {item=}")
+        return item
+
+    raise TuiSelectorError
+
+
+# def path_by_name(paths: list[Path]):
