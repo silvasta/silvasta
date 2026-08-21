a65edd0b57b42e0d030345d13691ccc587527e63
silvan <silvasta3@gmail.com>
2026-08-21 16:52:29 +0200

    ArgCaster

--- src/sstcore/brick/func/_MOVE_arg_decorator.py
+++ src/sstcore/brick/func/_MOVE_arg_decorator.py
@@ -3,29 +3,7 @@ import inspect
 from collections.abc import Callable
 from typing import Any
 
-from ..port.color import Color
-
-
-def resolve_color(color_guess: Any, default: Color | None = None) -> Color:
-    """Map [int|str|Color] to Color or default or Raise"""
-    try:
-        match color_guess:
-            case Color():
-                return color_guess
-
-            case int() as index:
-                return Color(index)
-
-            case str() as name:
-                return Color[name.upper()]
-
-            case _:
-                raise ValueError(f"Unrecognized type: {type(color_guess)}")
-
-    except (ValueError, KeyError) as error:
-        if default is not None:
-            return default
-        raise ValueError(f"Map Color failed: {color_guess=}") from error
+from ...port.color import Color
 
 
 def cast_color_args(func: Callable) -> Callable:
