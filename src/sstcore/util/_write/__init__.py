1ae2ece9929ea7439451fd8c26e9b4bb6245392d
silvan <silvasta3@gmail.com>
2026-08-23 16:11:05 +0200

    Scan

---
+++ src/sstcore/util/_write/__init__.py
@@ -0,0 +1,16 @@
+"""
+Transform Data from Runtime Status to something on Disk
+
+IDEAS:
+- Format and Style Text, Example: Summary File
+  Related: util.scan, util.tree, brick.format, brick.color
+
+- Overall, find proper encoding/decoding followed by fs operation
+  - Tree -> Text
+  - Stream Text -> Formatted Text
+    ...
+
+- Write Stub files
+Related: brick.forge, brick.view (minor)
+
+"""
--- src/sstcore/util/scan/__init__.py
+++ src/sstcore/util/scan/__init__.py
@@ -1,16 +1,42 @@
 """
 # Scanner - Detect the Target!
 
-## FolderScanner
-
+FolderScanner
 - Needs a Target Root and optional a FilterSet to Walk a directory
 - Provides Paths, builds a PathTree or creates a SummaryFile.
 
-## [FileScanner]
-
+FileScanner
 - Future project: detect the content of files
 
-                                                       DependencyLevel[X]
+---
+
+Strategy for Integrated Setup:
+- Task starts with 2 independant gathering steps:
+  - Get file paths: the easy setup, scan the folders and filter, already implemented
+  - Get file content: most simple setup: open file, read, provide str
+    - Here the complexity can grow very high... mainly in 2 layers:
+      - bit->text: binary files, pdfs, more difficult than just code...
+      - text->context: AST, CST, extract the meaning of the scans
+
+  Workflow will be, 1. organize Paths, 2. read data and 3. interprete
+  - arbitrary combinations possible:
+    - file paths exists already in a registry, file text, just step 3
+    - specific format is desired or available, focus on read
+
+That was the incoming side, everything until now was input processing.
+- Output is as well intended as 2 layers for now:
+  - Display Modus: e.g. existing TUI Monitor, nice base to build further
+  - Write Modus: here is safe file system writing implemented, nothing more
+    - Ok there exist a summary file writer, much more is maybe not even needed
+    - Except for the stub files! unsure if a new utils.write makes sense
+
+Process Managment by Scan - here the Core (maybe implemented as StaticFunc toolkit)
+- Some assembled class that controls:
+  - the 2 eyes of the system, path finder and the data reader
+  - the parsing and that the data actually ends up there
+  - finally forward everything to TUI/disk
+- config and emitter will arrive from CLI
+
 """
 
 __all__: list[str] = [
