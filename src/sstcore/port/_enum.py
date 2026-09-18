6306fbdfdf434959edc3a385fa9208c0794a06bd
silvan <silvasta3@gmail.com>
2026-09-18 06:32:53 +0200

    _Enum

---
+++ src/sstcore/port/_enum.py
@@ -0,0 +1,49 @@
+"""
+Govern the Shape and Structure of the Strict and Reliable Container
+
+                                                 DependencyLevel[0]
+"""
+
+# IDEAS: module name
+# - strict
+# - reliable
+# - relying
+# - define
+# - govern
+
+# STRATEGY: - General Setup
+# - evaluate on int|str|self
+# - str: name.lower() or shortcut
+
+# STRATEGY: - Axis: Implementation 1
+# - Grid:
+#  - ColorGrid
+#  - registry?
+# - int: range 0..n
+
+# STRATEGY: - Option: Implementation 2
+# - Policy:
+#   - PolicyDescriptor
+# - Select:
+#   - PrintOption
+# - int: default on 0
+
+# STRATEGY: - Catalog: Implementation 3
+# - Mixin
+# - FuncExecute
+# - int: 1 or slice[1...N] or even 0 for no select
+
+# NEXT: Transission? State? Graph?
+
+# TASK: base enum to derive
+# - __str__
+# - __repr__
+# - index
+# __getitem__
+
+
+# LATER:
+# ERROR: meta modified base enum
+
+
+# LATER: SstEnum - from scratch
