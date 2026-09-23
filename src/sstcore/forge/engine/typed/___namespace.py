ed51368923d056c6ed33fa47172314d7bf903361
silvan <silvasta3@gmail.com>
2026-09-23 20:11:48 +0200

    Forge.Engine.Typed

--- src/sstcore/forge/engine/typed/___namespace.py
+++ src/sstcore/forge/engine/typed/___namespace.py
@@ -1,16 +1,20 @@
 """
 Funcions to Assemble NameSpace for Classes
 
-                                                 DependencyLevel[0]
+                            DependencyLevel.sstcore.forge.engine[0]
 """
 
+from typing import Any
 
-def _rebase(cls: type, base: type) -> type:
-    ns = {
+type NameSpace = dict[str, Any]
+
+
+def rebase(cls: type, base: type) -> type:
+    ns: NameSpace = {
         k: v
         for k, v in cls.__dict__.items()
         if k not in {"__dict__", "__weakref__"}
-    }
+    }  # EXTRACT:
     ns["__module__"] = cls.__module__
     ns["__qualname__"] = getattr(cls, "__qualname__", cls.__name__)
     ns["__annotations__"] = dict(getattr(cls, "__annotations__", {}))
